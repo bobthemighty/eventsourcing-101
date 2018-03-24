b5e9b8dc66b9b4edd739843f0af4348f27d77aa2
Bob Gregory <bob@made.com>
2018-03-24 08:41:20 +0000

    remove code

--- mongobasket/baskets.py
+++ mongobasket/baskets.py
@@ -23,59 +23,11 @@ class Basket(Aggregate):
 
     # Deciders
 
-    @classmethod
-    def create(cls, basket_id):
-        basket = Basket()
-        basket.raise_event(events.BasketCreated(basket_id))
-        return basket
-
-    def add_item(self, product, qty):
-        self.raise_event(events.ItemAdded(self.id, product, qty))
-
-    def remove(self, product):
-        if product not in self.items:
-            raise KeyError()
-        self.raise_event(events.ItemRemoved(self.id, product))
-
     # Appliers
-
-    @applies(events.BasketCreated)
-    def on_created(self, event):
-        self.id = event.basket_id
-        self.items = Counter()
-
-    @applies(events.ItemAdded)
-    def on_added(self, event):
-        self.items[event.product] += event.qty
-
-    @applies(events.ItemRemoved)
-    def on_removed(self, event):
-        del self.items[event.product]
 
     # View methods
 
     def __str__(self):
         return print_basket(self)
 
-    def is_empty(self):
-        return not any(self.items)
-
-    def get_item(self, product):
-        return self.items[product]
-
     # Data Access
-
-    def save(self):
-        formatted_events = []
-        for e in self.new_events:
-            formatted_events.append(events.to_json(e))
-        db.baskets.insert(formatted_events)
-        self.new_events.clear()
-
-    @classmethod
-    def get(cls, basket_id):
-        data = db.baskets.find({'basket_id': basket_id})
-        read_events = []
-        for json in data:
-            read_events.append(events.from_json(json))
-        return Basket(read_events)
